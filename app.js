91ca84ef4dbbdfecae5cf2aaee08a9db0cee58fd
alasz <antti.laszlo@gmail.com>
2019-03-19 11:37:42 +0200

    second commit

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+var parser = bodyParser.urlencoded({ extended: true });
+app.use(bodyParser.json());
+var router = express.Router();
+//const fs = require('fs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
+const dbserv = require("./dbservice");
+var cors = require('cors');
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(bodyParser.urlencoded({ extended: true }));
 
-module.exports = app;
+// Get messages
+router.get('/getdata/:table_name', parser, function (req, res) {
+    console.log(req.params.table_name);
+     dbserv.getAllData(req.params.table_name).then(data => { res.json(data) });
+ })
+
+ // Post Quote
+ router.post('/addquote', parser, function (req, res) {
+    dbserv.addQuote(req).then(data => { (data => { res.json(data) }); }); 
+    //res.redirect('/messages/' +  req.body.table_name);
+})
+
+ app.use('/api', router);
+
+ let server = app.listen(3000, () => {
+     console.log(`Server listening on ${server.address().port}`);
+ });
---
+++ dbservice.js
@@ -0,0 +1,49 @@
+
+require('dotenv').config()
+
+const Pool = require('pg').Pool;
+
+const conopts = {
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  DATABASE_URL: 'postgres://lzmovuwmlnwgog:c392d8857c57cf3ab607586434c08cf445369d536b53c7833231c0f507a3fe09@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/dc7l8hdpr0tbic',
+  database: 'dc7l8hdpr0tbic'
+};
+
+const pool = new Pool(conopts);
+
+function getAllData(table_name) {
+  return pool.connect()
+    .then(client => {
+      let sql = "SELECT * FROM " + table_name + ";";
+      return client.query(sql)
+        .then(res => {
+          client.release();
+          return res.rows;
+        })
+        .catch(err => {
+          client.release();
+          throw error;
+        });
+    });
+};
+
+
+function addQuote(quote, response) {
+  console.log(quote)
+  return pool.connect()
+    .then(client => {
+      const kysely = {
+        text: "insert into generator(quote, class) values($1::text, $2::text) RETURNING *",
+        values: [quote.text, quote.class]
+      };
+      client.query(kysely, (error, results) => {
+        if (error) {
+          throw (error);
+        }
+        response.status(201).send(`Quote added`)
+      });
+    });
+};
+
+module.exports = { getAllData, addQuote };
